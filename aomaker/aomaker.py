7dbaa06a6b8dcf190a7557353cf7586df43d2ecd
wangfengji <wangfengji@datatower.ai>
2024-11-22 18:57:59 +0800

    feat:改造依赖接口装饰器

--- aomaker/aomaker.py
+++ aomaker/aomaker.py
@@ -1,5 +1,5 @@
 # --coding:utf-8--
-from aomaker._aomaker import dependence, async_api, update, command, hook, genson, data_maker, dataclass
+from aomaker._aomaker import dependence, async_api, update, command, hook, genson, data_maker, dataclass, kwargs_handle
 from aomaker.extension.retry.retry import retry, AoMakerRetry
 
 __all__ = [
@@ -12,5 +12,6 @@ __all__ = [
     'data_maker',
     'dataclass',
     'retry',
-    'AoMakerRetry'
+    'AoMakerRetry',
+    'kwargs_handle'
 ]
