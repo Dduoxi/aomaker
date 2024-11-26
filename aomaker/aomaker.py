f56e4e803ec39f8ca0ea541a8526993d3d496713
wangfengji <wangfengji@datatower.ai>
2024-11-26 09:42:35 +0800

    feat:case_handle装饰器，用于在用例层处理接口参数

--- aomaker/aomaker.py
+++ aomaker/aomaker.py
@@ -1,5 +1,5 @@
 # --coding:utf-8--
-from aomaker._aomaker import dependence, async_api, update, command, hook, genson, data_maker, dataclass, kwargs_handle, be_dependence
+from aomaker._aomaker import dependence, async_api, update, command, hook, genson, data_maker, dataclass, kwargs_handle, be_dependence, case_handle
 from aomaker.extension.retry.retry import retry, AoMakerRetry
 
 __all__ = [
@@ -14,5 +14,6 @@ __all__ = [
     'dataclass',
     'retry',
     'AoMakerRetry',
-    'kwargs_handle'
+    'kwargs_handle',
+    'case_handle'
 ]
