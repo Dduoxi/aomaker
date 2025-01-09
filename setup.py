076fdf3fc07f8867f66a24b14600479005c1ae1b
wangfengji <wangfengji@datatower.ai>
2025-01-09 15:14:28 +0800

    Feat:setup修正

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
-# --coding:utf-8--
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 setuptools.setup(
-    name="aomaker",
-    version="1.0.5",
-    author="fengjiTest",
-    author_email="w021221@yeah.net",
-    description="An api testing framework",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Dduoxi/aomaker",
-    packages=setuptools.find_packages(exclude=("aomaker", "aomaker.*")),
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    # 包的名称
+    name='aomaker',
+
+    # 包的版本
+    version='1.0.4',  # 请根据实际情况调整版本号
+
+    # 包的作
+    author='Dduoxi',
+
+    # 包的作者邮箱
+    author_email='your_email@example.com',  # 请替换为您的邮箱地址
+
+    # 包的描述
+    description='基于pytest的接口自动化测试框架',
+
+    # 包的长描述（可以从README文件中读取）
+    long_description=open('README.md').read(),
+
+    # 包的长描述内容类型
+    long_description_content_type='text/markdown',
+
+    # 包的URL
+    url='https://github.com/Dduoxi/aomaker',
+
+    # 包的依赖
     install_requires=[
         'black==23.10.0',
         'Jinja2==3.1.2',
@@ -38,13 +45,19 @@ setuptools.setup(
         'emoji==2.2.0',
         'click-help-colors==0.9.1',
         'tenacity==8.2.3',
-        'ruamel.yaml==0.17.21',
-        # 'tabulate==0.9.0',
-        'Faker~=33.0.0'
-        # 'fastapi==0.110.0',
-        # 'uvicorn==0.28.0'
-
+        'ruamel.yaml==0.17.21'
     ],
+
+    # 包的Python版本兼容性
+    python_requires='>=3.10',
+
+    # 包含的包和模块
+    packages=setuptools.find_packages(),
+
+    # 包含的额外文件（例如：README、LICENSE等）
+    include_package_data=True,
+
+    # 包的入口点
     entry_points={
         'console_scripts': [
             'amake=aomaker.cli:main_make_alias',
@@ -52,5 +65,23 @@ setuptools.setup(
             'arec=aomaker.cli:main_record_alias',
             'aomaker=aomaker.cli:main',
         ]
-    }
+    },
+
+    # 包的许可证
+    license='MIT',
+
+    # 包的分类
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    # 其他元数据
+    keywords='api automation pytest',
 )
